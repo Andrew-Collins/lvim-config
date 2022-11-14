af8b6b39415bd4b85b03f9dcd84060b1c9cebcf2
Andrew-Collins <andyc1367@gmail.com>
2022-11-14 13:49:36 +1000

    Debugger config

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -11,6 +11,11 @@ M.config = function()
       args = { "--port", "${port}" },
     },
   }
+  dap.adapters.cppdbg = {
+    id = 'cppdbg',
+    type = 'executable',
+    command = os.getenv("HOME") .. '/.local/share/nvim/mason/bin/OpenDebugAD7', -- adjust as needed
+  }
   dap.configurations.rust = {
     {
       name = "Default",
@@ -22,6 +27,26 @@ M.config = function()
       cwd = '${workspaceFolder}',
       stopOnEntry = false,
     },
+  }
+  dap.configurations.c = {
+    {
+      name                    = 'OpenOCD',
+      type                    = 'cppdbg',
+      request                 = 'launch',
+      MIMode                  = 'gdb',
+      miDebuggerServerAddress = 'localhost:3333',
+      miDebuggerPath          = '/opt/esp/tools/xtensa-esp32s3-elf/esp-2021r2-patch5-8.4.0/xtensa-esp32s3-elf/bin/xtensa-esp32s3-elf-gdb',
+      miDebuggerArgs          = '-x gdbinit',
+      cwd                     = '${workspaceFolder}',
+      program                 = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+      logging                 = {
+        trace = true,
+        traceResponse = true,
+        engineLogging = true
+      }
+    }
   }
 end
 return M
